545ab6e4dda4828bd5f4c98b086d97e98d6d0e93
Alex Kraus <ak555@byu.edu>
2022-06-09 10:31:58 -0600

    Batch job functionality from MASON

---
+++ main.py
@@ -0,0 +1,47 @@
+import os
+import subprocess
+import configparser
+
+from Commaker import commaker
+
+
+def main(configFile='config.ini'):
+
+    #region commaker input arguments
+    config = configparser.ConfigParser()
+    config.read(configFile)
+    basis_set = config['Files']['basis_set']
+    title = config['Properties']['title']
+    charge = config['Properties']['charge']
+    multiplicity = config['Properties']['multiplicity']    
+    procs = '%nprocshared='+config['Properties']['processors']+'\n'
+    mem = '%mem='+config['Properties']['memory']+'\n' 
+    header_path = config['Properties']['header']
+    #endregion
+
+    # Much of this is stolen from BatchBadenBond because I liked its output.
+    if not os.path.isdir('results'):
+        os.mkdir('results')
+
+    for file in os.listdir('inputs/'):
+        
+        full_filename = 'inputs/'+file
+        filename = file.split('.')[0]
+
+        if not os.path.isdir('results/'+filename):
+            os.mkdir('results/'+filename)
+        com_filename = 'results/'+filename+'/'+filename+'.com'
+        
+        commaker(full_filename, basis_set, title, charge, multiplicity, procs, mem,
+                 header_path, com_filename)
+        
+        subprocess.check_call('rung16 %s' (com_filename), shell=True)
+    
+        
+
+        
+        
+
+        
+
+main()
