9fe6696ca5f96fa899ca7e2f46ed5551375bbba6
Alex Kraus <ak555@byu.edu>
2022-06-16 12:02:07 -0600

    Fixes carbon charge.

--- Heterolysis_Homolysis.py
+++ Heterolysis_Homolysis.py
@@ -259,7 +259,7 @@ def charge_multiplicity(base_charge, base_multiplicity, split_type='homolysis'):
         metal_multiplicity = int(base_multiplicity) + 1
         carbon_multiplicity = 2 # Should be a doublet. 
         metal_charge = int(base_charge)
-        carbon_charge = int(base_charge)
+        carbon_charge = 0
     else:
         pass
 
